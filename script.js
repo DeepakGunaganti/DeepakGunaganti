a604356225bc6020e2e4a64ad6bde6c9d56133c7
DeepakGunaganti <141859536+DeepakGunaganti@users.noreply.github.com>
2023-08-27 05:00:44 +0000

    Add initial HTML, JavaScript, and CSS files git add index.html script.js style.css git commit -m "Add initial HTML, JavaScript, and CSS files" git push origin main

---
+++ script.js
@@ -0,0 +1,8 @@
+function color(){
+    const red = document.getElementById("red").value;
+    const green = document.getElementById("green").value;
+    const blue = document.getElementById("blue").value;
+    document.body.style.backgroundColor = 'rgb('+ red +',' + green +','+ blue +')';
+    const output = document.getElementById("output")
+    output.innerHTML = 'rgb('+ red +',' + green +','+ blue +')';
+}
